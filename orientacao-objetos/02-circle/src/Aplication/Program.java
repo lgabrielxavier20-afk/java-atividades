995ce4f9a1f9322d8b84d4c14956256e992ff1dc
Gabriel Xavier <lgabrielxavier20-afk@users.noreply.github.com>
2026-06-09 16:10:45 -0300

    Exercicio circle

---
+++ orientacao-objetos/02-circle/src/Aplication/Program.java
@@ -0,0 +1,24 @@
+﻿package Aplication;
+
+import Entity.Circle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+        Circle c = new Circle();
+
+        System.out.print("Enter radius: ");
+        c.radius = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f%n", c.area());
+        System.out.printf("CIRCUMFERENCE = %.2f%n", c.circumference());
+        System.out.printf("DIAMETER = %.2f%n", c.diameter());
+
+        sc.close();
+    }
+}
---
+++ orientacao-objetos/02-circle/src/Entity/Circle.java
@@ -0,0 +1,17 @@
+﻿package Entity;
+
+public class Circle {
+    public double radius;
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double circumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double diameter() {
+        return 2 * radius;
+    }
+}
